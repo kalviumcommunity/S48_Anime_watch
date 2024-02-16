1971e5af3b4fd70cf56b5752f8d1624dc3c40f27
Rajkiumar <sumathiselvam0007@gmail.com>
2024-02-16 11:07:30 +0530

    update with bruno api

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PUBLIC_PORT || 3000;
 
-// define the ping route with the response in JSON
+// Define the route for /ping with the response in JSON
 app.get('/ping', (req, res) => {
-  req.json({message:'pong'});
+  res.json({ message: 'pong' }); 
 });
 
 if (require.main === module) {
