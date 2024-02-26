0b00b64027777be59ba9afee112e16ff1666c36a
Rajkiumar <sumathiselvam0007@gmail.com>
2024-02-26 22:15:22 +0530

    Created signup page

--- frontend/react-client/src/App.jsx
+++ frontend/react-client/src/App.jsx
@@ -1,14 +1,14 @@
 // App.jsx
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LandingP from "./component/LandingPage";
+import LandingPage from "./component/LandingPage";
 import SignUp from "./component/Signup";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingP />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
